3dcf863c96b5fbfa43a71bbc503a7ad36d6434de
d.nishpal <danilnishpal512@gmail.com>
2024-02-26 18:49:38 +0300

    test

--- mixins/__init__.py
+++ mixins/__init__.py
@@ -1,4 +1,4 @@
-from application_exception import ApplicationException
-from service_mixin import ServiceMixin
-from model_mixin import CreatedUpdatedModelMixin, Base
+from utils.mixins.application_exception import ApplicationException
+from utils.mixins.service_mixin import ServiceMixin
+from utils.mixins.model_mixin import CreatedUpdatedModelMixin, Base
 __all__ = ("ApplicationException", "ServiceMixin", "CreatedUpdatedModelMixin", "Base",)
